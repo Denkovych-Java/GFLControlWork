accbc8ae4746cbdbf0daf413212f1a240dc27f9a
Oleh Denkovych <dnk.oleh@gmail.com>
2022-12-07 01:16:25 +0200

    second

---
+++ src/main/java/IO/EditFiles.java
@@ -0,0 +1,46 @@
+package IO;
+
+import manufacturer.Manufacturer;
+import souvenirs.Souvenir;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class EditFiles {
+
+    public static ArrayList<Manufacturer> manufacturersList = new ArrayList<>();
+    public static ArrayList<Souvenir> souvenirsList = new ArrayList<>();
+
+    public static void addManufacturer(Manufacturer manufacturer) {
+        manufacturersList.add(manufacturer);
+    }
+
+    public static void addSouvenir(Souvenir souvenir) {
+        souvenirsList.add(souvenir);
+        for (Manufacturer m : manufacturersList) {
+            if (m.getName().equals(souvenir.getManufacturer().getName())) return;
+        }
+    }
+
+    public static boolean deleteSouvenir(Souvenir souvenir) {
+        return souvenirsList.remove(souvenir);
+    }
+
+    public static ArrayList<Souvenir> getSouvenirs() {
+        return souvenirsList;
+    }
+
+    public static ArrayList<Manufacturer> getManufacturers() {
+        return manufacturersList;
+    }
+
+    public static void getSouvenirsFromManufacturer(String nameM){
+        if(checkSouvenirsFromManufacturer(nameM)){
+            souvenirsList.stream().filter(s -> s.getManufacturer().getName().equals(nameM)).forEach(Souvenir::print);
+        }
+    }
+    private static boolean checkSouvenirsFromManufacturer(String nameM){
+        return manufacturersList.stream().anyMatch(m -> m.getName().equals(nameM));
+    }
+}
