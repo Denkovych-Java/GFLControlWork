4692d33e6eb07001d0708cc84c9b5b856a84ffcc
Oleh Denkovych <dnk.oleh@gmail.com>
2022-12-07 02:08:50 +0200

    second

--- src/main/java/IO/EditFiles.java
+++ src/main/java/IO/EditFiles.java
@@ -3,9 +3,9 @@ package IO;
 import manufacturer.Manufacturer;
 import souvenirs.Souvenir;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.Calendar;
 
 public class EditFiles {
 
@@ -35,21 +35,44 @@ public class EditFiles {
         return manufacturersList;
     }
 
-    public static void getSouvenirsFromManufacturer(String nameM){
-        if(checkSouvenirsFromManufacturer(nameM)){
+    public static void getSouvenirsFromManufacturer(String nameM) {
+        if (checkSouvenirsFromManufacturer(nameM)) {
             souvenirsList.stream().filter(s -> s.getManufacturer().getName().equals(nameM)).forEach(Souvenir::print);
         }
     }
-    private static boolean checkSouvenirsFromManufacturer(String nameM){
+
+    private static boolean checkSouvenirsFromManufacturer(String nameM) {
         return manufacturersList.stream().anyMatch(m -> m.getName().equals(nameM));
     }
 
-    public static void getSouvenirsFromCountry(String country){
-        if(checkSouvenirsFromCountry(country)){
+    public static void getSouvenirsFromCountry(String country) {
+        if (checkSouvenirsFromCountry(country)) {
             souvenirsList.stream().filter(s -> s.getManufacturer().getCountry().equals(country)).forEach(Souvenir::print);
         }
     }
-    private static boolean checkSouvenirsFromCountry(String country){
+
+    private static boolean checkSouvenirsFromCountry(String country) {
         return manufacturersList.stream().anyMatch(m -> m.getCountry().equals(country));
     }
+
+    public static void getManufacturersWithLowerPrice(BigDecimal price) {
+        manufacturersList.forEach(m -> souvenirsList.stream().filter(s -> s.getPrice().compareTo(price) < 0).forEach(s -> m.print()));
+    }
+
+    public static void getManufacturersWithSouvenirs() {
+        for (Manufacturer m : manufacturersList) {
+            System.out.println("Manufacturer: ");
+            m.print();
+            getSouvenirsFromManufacturer(m.getName());
+        }
+    }
+
+    public static void getManufacturerWithSouvenirsYears(String nameS, int year){
+        System.out.println("Souvenir: ");
+        souvenirsList.stream().filter(s -> s.getName().equals(nameS) && s.getDate().get(Calendar.YEAR) == year).forEach(s -> {
+            System.out.println("Manufacturer: ");
+            s.getManufacturer().print();
+        });
+    }
+
 }
