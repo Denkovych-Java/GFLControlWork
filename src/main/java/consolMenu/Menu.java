02e770187c894f058c6ef639479a2af09d744da9
Oleh Denkovych <dnk.oleh@gmail.com>
2022-12-08 14:59:56 +0200

    second

--- src/main/java/consolMenu/Menu.java
+++ src/main/java/consolMenu/Menu.java
@@ -9,6 +9,8 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
+import java.util.stream.IntStream;
+
 
 
 public class Menu {
@@ -33,7 +35,7 @@ public class Menu {
         System.out.println("Edit:      2");
         System.out.println("Print:     3");
         System.out.println("Exit:      0");
-        switch (getStr()) {
+        switch (getInt()) {
             case 1:
                 menuAdd();
                 break;
@@ -48,6 +50,19 @@ public class Menu {
             default:
                 System.out.println("default");
                 getFirstMenu();
+        }
+    }
+
+    private static void menuPrint() {
+        ArrayList<Souvenir> souvenirs = EditFiles.getSouvenirs();
+        ArrayList<Manufacturer> manufacturers = EditFiles.getManufacturers();
+        System.out.println("All manufacturers:");
+        for (Manufacturer manufacturer : manufacturers){
+            manufacturer.print();
+        }
+        System.out.println("All souvenirs:");
+        for (Souvenir souvenir : souvenirs){
+            souvenir.print();
         }
     }
 
@@ -67,6 +82,7 @@ public class Menu {
                 menuAdd();
         }
     }
+
     private static void addManufacturer() {
         String nameManufacturer;
         String country;
@@ -76,6 +92,7 @@ public class Menu {
         country = getStr();
         EditFiles.addManufacturer(new Manufacturer(nameManufacturer, country));
     }
+
     private static void addSouvenir() {
         String nameSouvenir;
         String nameManufacturer;
@@ -98,6 +115,7 @@ public class Menu {
         price = getBD();
         EditFiles.addSouvenir(new Souvenir(nameSouvenir, new Manufacturer(nameManufacturer, country), new GregorianCalendar(year, month, day), price));
     }
+
     private static void menuEdit() {
         printSelector();
         switch (getInt()) {
@@ -117,22 +135,101 @@ public class Menu {
         }
     }
 
+    private static void editManufacturer() {
+        String nameManufacturer;
+        String country;
+        ArrayList<Manufacturer> manufacturers = EditFiles.getManufacturers();
+        IntStream.range(0, manufacturers.size()).mapToObj(i -> "[" + i + "] manufacturer").forEachOrdered(System.out::println);
+        System.out.println("Select a souvenir to edit: ");
+        int selectM = getInt();
+        manufacturers.get(selectM).print();
+        System.out.println("Select what you want to edit: ");
+        System.out.println("Name                         1");
+        System.out.println("Country                      2");
+        System.out.println("Exit                         0");
+        int selectToEdit = getInt();
+        switch (selectToEdit) {
+            case 1:
+                System.out.print("Enter name: ");
+                nameManufacturer = getStr();
+                manufacturers.get(selectM).setName(nameManufacturer);
+                break;
+            case 2:
+                System.out.print("Enter country: ");
+                country = getStr();
+                manufacturers.get(selectM).setCountry(country);
+                break;
+            case 0:
+                return;
+            default:
+                System.out.println("The manufacturer is unedited. Please select 1-2");
+                editSouvenir();
+        }
+    }
+
+
     private static void editSouvenir() {
-       String nameSouvenir;
-       String nameManufacturer;
-       String country = null;
-       int day;
-       int month;
-       int year;
-       BigDecimal price;
+        String nameSouvenir;
+        String nameManufacturer;
+        String country = null;
+        int day;
+        int month;
+        int year;
+        BigDecimal price;
         ArrayList<Souvenir> souvenirs = EditFiles.getSouvenirs();
-        
+        IntStream.range(0, souvenirs.size()).mapToObj(i -> "[" + i + "] souvenir").forEach(System.out::println);
+        System.out.println("Select a souvenir to edit: ");
+        int selectS = getInt();
+        souvenirs.get(selectS).print();
+        System.out.println("Select what you want to edit: ");
+        System.out.println("Name                         1");
+        System.out.println("Manufacturer                 2");
+        System.out.println("Date                         3");
+        System.out.println("Price                        4");
+        System.out.println("Exit                         0");
+        int selectToEdit = getInt();
+        switch (selectToEdit) {
+            case 1:
+                System.out.print("Enter name: ");
+                nameSouvenir = getStr();
+                souvenirs.get(selectS).setName(nameSouvenir);
+                break;
+            case 2:
+                System.out.print("Print manufacturer name: ");
+                nameManufacturer = getStr();
+                for (Manufacturer manufacturer : EditFiles.getManufacturers()) {
+                    if (manufacturer.getName().equals(nameManufacturer)) {
+                        country = manufacturer.getCountry();
+                    }
+                }
+                souvenirs.get(selectS).setManufacturer(new Manufacturer(nameManufacturer, country));
+                break;
+            case 3:
+                System.out.print("Enter day: ");
+                day = getInt();
+                System.out.print("Enter month: ");
+                month = getInt();
+                System.out.print("Enter year: ");
+                year = getInt();
+                souvenirs.get(selectS).getDate().set(year, month, day);
+                break;
+            case 4:
+                System.out.print("Print price: ");
+                price = getBD();
+                souvenirs.get(selectS).setPrice(price);
+                break;
+            case 0:
+                return;
+            default:
+                System.out.println("The souvenir is unedited. Please select 1-4");
+                editSouvenir();
+        }
     }
 
     private static void printSelector() {
-       System.out.println("Souvenir:    1");
-       System.out.println("Manufacturer:        2");
-       System.out.println("Exit:            0");
+        System.out.println("Souvenir:    1");
+        System.out.println("Manufacturer:        2");
+        System.out.println("Exit:            0");
     }
 
 
